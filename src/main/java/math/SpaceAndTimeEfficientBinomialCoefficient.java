0425827c9316fc22b571a67c84df60cd736449d4
Mukesh Bansiwal <mbbansiw@amazon.com>
2019-08-04 09:56:42 +0530

    Added sum of binomial coefficient or power set

---
+++ src/main/java/math/SpaceAndTimeEfficientBinomialCoefficient.java
@@ -0,0 +1,38 @@
+package math;
+
+/**
+ * https://www.geeksforgeeks.org/space-and-time-efficient-binomial-coefficient/
+ *
+ * Write a function that takes two parameters n and k and returns the value of Binomial Coefficient C(n, k). For example, your function should return 6 for n = 4 and k = 2, and it should return 10 for n = 5 and k = 2.
+ *
+ * C(n, k) = n! / (n-k)! * k!
+ *         = [n * (n-1) *....* 1]  / [ ( (n-k) * (n-k-1) * .... * 1) *
+ *                                     ( k * (k-1) * .... * 1 ) ]
+ * After simplifying, we get
+ * C(n, k) = [n * (n-1) * .... * (n-k+1)] / [k * (k-1) * .... * 1]
+ *
+ * Also, C(n, k) = C(n, n-k)  // we can change r to n-r if r > n-r
+ *
+ */
+public class SpaceAndTimeEfficientBinomialCoefficient {
+    public static int calculate(int n, int k){
+        if(k > (n - k)){
+            k = n - k;
+        }
+        int value = 1;
+
+        for(int i = 0; i < k; ++i){
+            value *= (n-i);
+            value /= (i+1);
+        }
+        return value;
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 8;
+        int k = 2;
+        System.out.println("Value of C("+ n + ", " + k+ ") "
+                + "is" + " "+ calculate(n, k));
+    }
+}
